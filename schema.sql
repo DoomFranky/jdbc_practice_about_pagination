4abd85b530d21209472729033e75c467b53c0ff0
Randy <raoulrandy3@gmail.com>
2025-12-06 16:47:13 +0300

    change the price value

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE Product (
+CREATE TABLE "Product" (
     id INT PRIMARY KEY,
     "name" VARCHAR,
-    price INT,
+    price NUMERIC(10,2),
     creation_product TIMESTAMP
 );
 
-CREATE TABLE Product_category (
+CREATE TABLE "Product_category" (
     id INT PRIMARY KEY,
     "name" VARCHAR,
     product_id INT,
-    CONSTRAINT fk_product_product_category FOREIGN KEY (product_id) REFERENCES Product(id)
+    CONSTRAINT fk_product_product_category FOREIGN KEY (product_id) REFERENCES "Product"(id)
 );
