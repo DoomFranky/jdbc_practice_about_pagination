ff8afb186968b180d7569cf92b7764252f93c1b3
Randy <raoulrandy3@gmail.com>
2025-12-06 17:41:25 +0300

    add Category class

---
+++ src/main/java/Product_management/Category.java
@@ -0,0 +1,19 @@
+package Product_management;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
