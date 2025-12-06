f80be09331f368055dd2a539b321da2f05f05949
Randy <raoulrandy3@gmail.com>
2025-12-06 21:17:46 +0300

    add product list paged

--- src/main/java/Product_management/DataRetriever.java
+++ src/main/java/Product_management/DataRetriever.java
@@ -1,6 +1,9 @@
 package Product_management;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +13,10 @@ public class DataRetriever {
         DBConnection dbconnection = new DBConnection();
         Connection connection = dbconnection.getDBConnection();
         try{
-            PreparedStatement statement = connection.prepareStatement("""
+            PreparedStatement preparedStatement = connection.prepareStatement("""
                 SELECT id,name FROM "Product_category"
                     """);
-            ResultSet rs = statement.executeQuery();
+            ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
                 Category category =  new Category(rs.getInt("id"), rs.getString("name"));
                 allCategories.add(category);
@@ -30,4 +33,50 @@ public class DataRetriever {
         return allCategories;
     }
 
+    public List<Product> getProductList(int page, int size){
+        List<Product> productList = new ArrayList<>();
+        DBConnection dbconnection = new DBConnection();
+        Connection connection = dbconnection.getDBConnection();
+
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement("""
+                    SELECT product.id AS product_id,
+                        product.name AS product_name, 
+                        product.creation_product, 
+                        category.id AS category_id,
+                        category.name AS category_name
+                    FROM "Product" AS product
+                    JOIN "Product_category" AS category
+                    ON category.product_id = product.id 
+                    LIMIT ? OFFSET ?
+                    """);
+            preparedStatement.setInt(1, size);
+            preparedStatement.setInt(2, (page-1)*size);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Product product =  new Product(
+                    rs.getInt("product_id"),
+                    rs.getString("product_name"),
+                    LocalDateTime
+                        .parse(rs.getString("creation_product"),DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                        .atZone(ZoneId.of("UTC"))
+                        .toInstant()
+                    ,new Category(
+                        rs.getInt("category_id"), 
+                        rs.getString("category_name")
+                    )
+                );
+                productList.add(product);
+            }
+            connection.close();
+        }catch (SQLException e){
+            try{
+                connection.close();
+            }catch(SQLException sqlE){
+                sqlE.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        return productList;
+    }
 }
